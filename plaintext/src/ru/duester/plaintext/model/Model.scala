02410626a99d0da60eb1bb0103bce950bc720a49
m.dyuster <m.dyuster@tinkoffinsurance.ru>
2025-08-10 01:10:52 +0300

    metadata

--- plaintext/src/ru/duester/plaintext/model/Model.scala
+++ plaintext/src/ru/duester/plaintext/model/Model.scala
@@ -1,5 +1,8 @@
 package ru.duester.plaintext.model
 
-case class PlainTextDocument(paragraphs: List[Paragraph])
+case class PlainTextDocument(
+    paragraphs: List[Paragraph],
+    metadata: Map[String, String] = Map.empty
+)
 
 case class Paragraph(text: String)
